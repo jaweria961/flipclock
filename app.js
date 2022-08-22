f150332aebab8b2400124e3e4c63af50a1a6e1f5
jaweria961 <javeriahanif65@gmail.com>
2022-08-22 11:25:46 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+let Flipper = function() {
+	function Flipper(node, currentTime, nextTime) {
+		this.isFlipping = false;
+		this.duration = 600;
+		this.flipNode = node;
+		this.frontNode = node.querySelector(".front");
+		this.backNode = node.querySelector(".back");
+		this.setFrontTime(currentTime);
+		this.setBackTime(nextTime);
+	}
+	Flipper.prototype.setFrontTime = function (time) {
+		this.frontNode.dataset.number = time;
+	};
+	Flipper.prototype.setBackTime = function (time) {
+		this.backNode.dataset.number = time;
+	};
+	Flipper.prototype.flipDown = function (currentTime, nextTime) {
+		var _this = this;
+
+		if (this.isFlipping) {
+			return false;
+		}
+
+		this.isFlipping = true;
+		this.setFrontTime(currentTime);
+		this.setBackTime(nextTime);
+		this.flipNode.classList.add("running");
+		setTimeout(function() {
+			_this.flipNode.classList.remove("running");
+			_this.isFlipping = false;
+			_this.setFrontTime(nextTime);
+		}, this.duration);
+	};
+	return Flipper;
+}();
+
+let getTimeFromDate = function (date) {
+	return date.toTimeString().slice(0, 8).split(":").join("");
+};
+
+let flips = document.querySelectorAll(".flip_clock");
+let now = new Date();
+let nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+let nextTimeStr = getTimeFromDate(now);
+let Flippers = Array.from(flips).map(function (flip, i) {
+	return new Flipper(flip, nowTimeStr[i], nextTimeStr[i]);
+});
+
+setInterval(function () {
+	let now = new Date();
+	let nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+	let nextTimeStr = getTimeFromDate(now);
+
+	for (let i = 0; i < Flippers.length; i++) {
+		if (nowTimeStr[i] === nextTimeStr[i]) {
+			continue;
+		}
+		Flippers[i].flipDown(nowTimeStr[i], nextTimeStr[i]);
+	}
+}, 1000);
